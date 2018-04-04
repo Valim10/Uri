834770a81978086fb60533ec9b7995013e6ab528
Daniel Valim <dvalim10@gmail.com>
2018-04-04 14:14:35 -0300

    Create 1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+using namespace std;
+ 
+int main() {
+    int a1,a2;
+    scanf("%d",&a1);
+    scanf("%d",&a2);
+
+    printf("SOMA = %d\n",a1+a2);
+    return 0;
+}
