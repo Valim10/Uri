a163acbaee13d7aaa68bae2f2f39df27ba0f5675
Daniel Valim <dvalim10@gmail.com>
2018-04-04 14:13:26 -0300

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+using namespace std;
+ 
+int main() {
+    double a1;
+    scanf("%lf",&a1);
+    double b =a1*a1*3.14159;
+    printf("A=%.4lf\n",b);
+    return 0;
+}
