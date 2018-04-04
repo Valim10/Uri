f067ca5c4a6974c8679f21b084f233bc60855024
Daniel Valim <dvalim10@gmail.com>
2018-04-04 14:07:22 -0300

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+
+using namespace std;
+ 
+int main() {
+    int a1,a2;
+    scanf("%d",&a1);
+    scanf("%d",&a2);
+
+    printf("X = %d\n",a1+a2);
+    return 0;
+}
